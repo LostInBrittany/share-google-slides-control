0852b0601db83dca8b80099ef65fc33eb6e7ca1b
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2024-07-16 16:35:25 +0200

    Adding Chrome Extension

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ This project empowers presenters by allowing them to share control of a Google S
 
 ## How It Works
 
-The solution involves a Greasemonkey userscript on the presenter's side that connects to a remote server via WebSocket. This setup facilitates the following features:
+The solution involves either a Chrome Extension or a Greasemonkey/Tampermonkey Userscript on the presenter's side that connects to a remote server via WebSocket. This setup facilitates the following features:
 
 - **QR Code and Link Generation**: The userscript generates a QR code or a link that can be shared with co-presenters.
 - **Remote Control Interface**: Co-presenters can use the provided link to access a user interface that allows them to control the slide deck in real-time.
@@ -23,8 +23,6 @@ https://github.com/user-attachments/assets/befd48ca-fcbe-49ce-ac8e-0cbc6caf2dde
 
 - **Seamless Integration**: Easily integrate with your existing Google Slides presentations.
 - **Real-Time Collaboration**: Multiple presenters can control slide transitions without interruptions.
-
-
 - **Secure Connections**: Utilizes WebSocket connections to ensure real-time, secure control sharing.
 
 ## Usage
@@ -33,15 +31,25 @@ If you simply want to use this project to share your slides, I have deployed an 
 
 In order to use it you will need to follow this steps:
 
+1. **Presenter Setup**: The main presenter installs the userscript or the extension and initiates the presentation.
+2. **Share Control**: The presenter shares the generated QR code or link with co-presenters.
+3. **Co-presenters Control**: Co-presenters access the control interface through the shared link and can control slide transitions in real-time.
+
+
+### Installing the Userscript
+
 1. **Install Userscripts plugin in your browser**: the most populars are [Tampermonkey](https://tampermonkey.net/) and [Greasemonkey](http://www.greasespot.net/).
 2. **Add the Userscript**: directly from [GitHub](./userscript/shared-google-slides-control.user.js), or from the [GreasyFork page](https://greasyfork.org/en/scripts/500807-share-google-slides-controls).
 3. **Run the Script**: Open your Google Slides presentation, and the script will automatically display a QR code and link for sharing control.
-4. **Share Control**: The presenter shares the generated QR code or link with co-presenters.
-5. **Co-presenters Control**: Co-presenters access the control interface through the shared link and can control slide transitions in real-time.
 
-## Installation
 
-You can deploy the server in any host accepting NodeJS. You will need to change the Userscript as it points to the [Clever Cloud](https://clever-cloud.com) instance of the server by default.
+### Installing the extension
+
+The Chrome Extension isn't in the store yet, you need to manually load it in unpacked format from the `chrome-extension` folder, [as described in Chrome Extensions documentation](https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world#load-unpacked). 
+
+## Deploying the server elsewhere
+
+You can deploy the server in any host accepting NodeJS. You will need to change the Userscript and the Chrome Extension as they point to the [Clever Cloud](https://clever-cloud.com) instance of the server.
 
 ## Contribution
 
---
+++ chrome-extension/scripts/content.js
@@ -0,0 +1,132 @@
+(async function() {
+  
+  const {html, render} = await import('./lit-html.js');
+
+  async function getQRCode() {
+      let resp = await fetch("https://shared-google-slides-control.cleverapps.io/qrcode", {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({url:`https://shared-google-slides-control.cleverapps.io/ui/#${slidedeck}`}),
+      });
+      let generatedQRCode = await resp.text();
+      return generatedQRCode;
+  }
+
+  function showDialog() {
+      const dialog = html`<dialog id="shareDialog">
+            <form method="dialog">
+              <h1>Remote control URL</h1>
+              <p>
+                <a href="https://shared-google-slides-control.cleverapps.io/ui/#${slidedeck}">
+                  https://shared-google-slides-control.cleverapps.io/ui/#${slidedeck}
+                </a>
+              </p>
+              <div style="display:flex;flex-flow:row;justify-content:center;"><img src="${generatedQRCode}"></div>
+              <div style="display:flex;flex-flow:row;justify-content:center;gap:2rem;">
+                <button autofocus id="copyDialog" style="padding:0.5rem;min-width:10rem">Copy</button>
+                <button id="closeDialog" style="padding:0.5rem;min-width:10rem">Close</button>
+              </div>
+            </form>
+          </dialog>`;
+
+      let dialogParent = document.createElement('div');
+      document.body.appendChild(dialogParent);
+      render(dialog, dialogParent);
+      document.getElementById('shareDialog').showModal();
+
+      const closeButton = document.getElementById("closeDialog");
+      closeButton.addEventListener("click", () => {
+          document.getElementById('shareDialog').close();
+      });
+      const copyButton = document.getElementById("copyDialog");
+      copyButton.addEventListener("click", () => {
+          console.log(`Link:  https://shared-google-slides-control.cleverapps.io/ui/#${slidedeck}`);
+          navigator.clipboard.writeText(`https://shared-google-slides-control.cleverapps.io/ui/#${slidedeck}`);
+      });
+  }
+
+  // Function to send a keyboard event
+  function sendKeyboardEvent(key, code, keyCode, element = document.body) {
+      const event = new KeyboardEvent('keydown', {
+          key: key,
+          code: code,
+          keyCode: keyCode,
+          charCode: keyCode,
+          which: keyCode,
+          bubbles: true,
+          cancelable: true
+      });
+
+      element.dispatchEvent(event);
+      console.log(`Sent keyboard event: ${key}`);
+  }
+
+  function connectWebSocket() {
+      socket = new WebSocket(wsUrl);
+
+      socket.addEventListener('open', function(event) {
+          console.log('WebSocket connection opened:', event);
+          reconnectInterval = 1000; // Reset reconnect interval on successful connection
+
+
+      });
+
+      // Event listener for receiving messages from the server
+      socket.addEventListener('message', function (event) {
+          console.log('Message from server:', event.data);
+
+          switch (event.data) {
+              case 'next':
+                  sendKeyboardEvent('ArrowRight', 'ArrowRight', 39);
+                  break;
+              case 'previous':
+                  sendKeyboardEvent('ArrowLeft', 'ArrowLeft', 37);
+                  break;
+
+          }
+      });
+
+      // Event listener for when the connection is closed
+      socket.addEventListener('close', function (event) {
+          console.log('WebSocket connection closed:', event);
+          attemptReconnect();
+      });
+
+      socket.addEventListener('error', function(event) {
+          console.error('WebSocket error:', event);
+          socket.close(); // Close the socket on error to trigger the reconnect logic
+      });
+  }
+
+  function attemptReconnect() {
+      console.log(`Attempting to reconnect in ${reconnectInterval / 1000} seconds...`);
+      setTimeout(function() {
+          reconnectInterval = Math.min(reconnectInterval * 2, maxReconnectInterval); // Exponential backoff
+          connectWebSocket();
+      }, reconnectInterval);
+  }
+
+  // Get the pathname of the current page
+  const pathname = window.location.pathname;
+
+  const slidedeck = pathname.replace('/presentation/d/','').split('/')[0];
+
+  console.log(`Presentation Id: ${slidedeck}`);
+
+  let generatedQRCode = await getQRCode();
+  showDialog();
+
+  // URL of the WebSocket server
+  const wsUrl = `wss://shared-google-slides-control.cleverapps.io/websocket/${slidedeck}`;
+
+
+  let socket;
+  let reconnectInterval = 1000; // Start with a 1 second delay
+  const maxReconnectInterval = 30000; // Maximum delay of 30 seconds
+
+
+  // Initial WebSocket connection
+  connectWebSocket();
+})();
